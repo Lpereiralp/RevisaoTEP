fda98e615d3e785684f70089d02d2498bb72e231
Otaviano <otaviano@cotemig.com.br>
2021-02-26 17:16:59 -0300

    Adicionar arquivos de projeto.

---
+++ RevisaoTEP/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace RevisaoTEP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnValida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(25, 55);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(160, 22);
+            this.txtsenha.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Atividade 1 - Senha";
+            // 
+            // btnValida
+            // 
+            this.btnValida.Location = new System.Drawing.Point(25, 83);
+            this.btnValida.Name = "btnValida";
+            this.btnValida.Size = new System.Drawing.Size(160, 23);
+            this.btnValida.TabIndex = 2;
+            this.btnValida.Text = "Validar Senha";
+            this.btnValida.UseVisualStyleBackColor = true;
+            this.btnValida.Click += new System.EventHandler(this.btnValida_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 512);
+            this.Controls.Add(this.btnValida);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsenha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnValida;
+    }
+}
+
---
+++ RevisaoTEP/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevisaoTEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValida_Click(object sender, EventArgs e)
+        {
+            int senha;
+            senha = int.Parse(txtsenha.Text);
+            if (senha == 1234)
+            {
+                MessageBox.Show("Acesso Permitido");
+            }
+            else
+            {
+                MessageBox.Show("Acesso Negado");
+            }
+        }
+    }
+}
